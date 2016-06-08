406d4fff94c5c530aadcb809588f291116ad3035
Jonas Kuche <Jonas.kuche@gmail.com>
2016-06-08 15:22:14 +0200

    Integrate YouTube Video and Input Field directive

--- public/src/js/directives/YouTubeVideo.js
+++ public/src/js/directives/YouTubeVideo.js
@@ -8,7 +8,20 @@ frontendApp.directive('cleverYoutube', function() {
       height: '=height'
     },
     controller: function($scope, $sce) {
-      $scope.url = $sce.trustAs($sce.RESOURCE_URL, $scope.iurl);
+      var patt = /https:\/\/www\.youtube\.com\/watch\?v=(.*)/;
+
+      var test = patt.test($scope.iurl);
+
+      if (test == true) {
+        var id = patt.exec($scope.iurl)[1];
+        $scope.eurl = "https://www.youtube.com/embed/" + id;
+      }
+      if (test== false) {
+        $scope.urlError = true;
+      }
+      $scope.url = $sce.trustAs($sce.RESOURCE_URL, $scope.eurl);
+      $scope.width = 420;
+      $scope.height = 315;
     }
   };
 });
