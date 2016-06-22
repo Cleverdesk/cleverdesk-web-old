92d692860e7961db6d3c22bb4d0fb2068308c7ed
Jonas Kuche <Jonas.kuche@gmail.com>
2016-06-22 18:58:22 +0200

    Add auth service

---
+++ public/src/js/services/auth.js
@@ -0,0 +1,21 @@
+frontendApp.factory('auth', function($http) {
+    return {
+        auth: function(username, password) {
+            return new Promise(function(resolve, reject) {
+                $http({
+                    method: 'POST',
+                    url: '/backend/auth',
+                    data: {"username": username, 'password': password, 'lifetime': 86400}
+                }).then(function successCallback(response) {
+                    if(response.data.status == 200) {
+                        resolve(response.data.body);
+                    } else {
+                        reject("Non 200 Status ("+response.data.status+")");
+                    }
+                }, function errorCallback(response) {
+                    reject("Error ("+response.status+")");
+                });
+            });
+        }
+    }
+});
