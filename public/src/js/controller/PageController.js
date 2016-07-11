57ce88e38e1dc4f4caf180f6fd9d27d79161e618
Jonas Kuche <Jonas.kuche@gmail.com>
2016-07-11 12:44:25 +0200

    Add identifer

--- public/src/js/controller/PageController.js
+++ public/src/js/controller/PageController.js
@@ -1,6 +1,7 @@
 frontendApp.controller('PageController', function($scope, $log, $routeParams, communicator, parser) {
     communicator.getPage($routeParams.plugin, $routeParams.page).then(function(page) {
-    parser.parsePage(page).then(function(components) {
+        $log.log(page);
+        parser.parsePage(page).then(function(components) {
             $scope.components = components;
             $scope.$apply();
         });
--- public/src/js/services/parser.js
+++ public/src/js/services/parser.js
@@ -2,6 +2,7 @@ frontendApp.factory('parser', function($sce) {
     return {
         parsePage: function(page) {
             return new Promise(function(resolve, reject) {
+                page = JSON.parse(page);
                 var components = page.components;
                 resolve(components);
             });
